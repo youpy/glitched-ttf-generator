474c4a4e226a209f9b81078b3c5a470a9de8a6d5
youpy <youpy@buycheapviagraonlinenow.com>
2011-09-02 20:13:15 +0900

    imprement

--- glitched_ttf_generator.rb
+++ glitched_ttf_generator.rb
@@ -1,6 +1,36 @@
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 get '/' do
-  'Hello, world!'
+  haml :index
+end
+
+put '/upload' do
+  if params[:file]
+    content_type params[:file][:type]
+    attachment 'glitched.ttf'
+
+    f = params[:file][:tempfile]
+    data = f.read
+
+    offset = glyph_offset(data)
+    length = glyph_length(data)
+
+    head = data[0, offset]
+    glyphs = data[offset, length]
+    other = data[offset + length .. -1]
+
+    glyphs.gsub!(/\w/, '0')
+
+    head + glyphs + other
+  end
+end
+
+def glyph_offset(data)
+  data[data.index('glyf') + 8, 4].unpack("N")[0]
+end
+
+def glyph_length(data)
+  data[data.index('glyf') + 12, 4].unpack("N")[0]
 end
